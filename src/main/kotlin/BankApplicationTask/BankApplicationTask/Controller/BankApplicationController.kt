eb276f03a38d17a3fcd0547f01c5aebc87de0f07
yrnam <yrnam@yrnamui-noteubug.local>
2023-04-13 09:42:25 +0900

    update task1

--- src/main/kotlin/BankApplicationTask/BankApplicationTask/Controller/BankApplicationController.kt
+++ src/main/kotlin/BankApplicationTask/BankApplicationTask/Controller/BankApplicationController.kt
@@ -9,33 +9,32 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
+@RequestMapping("/users", produces =["application/json; charset=UTF-8"] )
 class BankApplicationController(private val bankApplicationService : BankApplicationService) {
-    private val logger =KotlinLogging.logger {  }
+    private val logger =KotlinLogging.logger {  } //로그 라이브러리 사용
 
-
-        @GetMapping("/users")
-        fun test():String{
-               logger.debug("Test 작동?")
-                return "정상작동"
-        }
-
-
-        //사용자 생성을 하는 컨트롤러
-       @PostMapping("/users/{id}")
+        //사용자 생성 api
+       @PostMapping("/{id}")
         fun createUsers(@PathVariable("id") userId :String,@RequestBody password:String ) : ResponseEntity<Users>{
-                logger.debug ("createUsers - userId : $userId, password : $password") // 나중에 ? 지우기 ->
-                //서비스의 메소드 호출
-                val iscreated  = bankApplicationService.createUsers(userId,password) //사용자 생성을 위한 service메소드 호출
+                logger.debug ("createUsers - userId : $userId, password : $password") //로거
+
+                //사용자 생성을 위한 service메소드 호출
+                val iscreated  = bankApplicationService.createUsers(userId,password)
                 logger.debug ("iscreated : $iscreated")
+
+                //응답 처리
                 if( iscreated != null ){ // 사용자 정보가 생성이 됐다면 Reponse: OK , body : 없음
                          return ResponseEntity(HttpStatus.OK)
                 }else {//생성이 되지 않았다면 Reponse :badrequest
                         return ResponseEntity(HttpStatus.BAD_REQUEST)
                 }
         }
+
+
 
 }
