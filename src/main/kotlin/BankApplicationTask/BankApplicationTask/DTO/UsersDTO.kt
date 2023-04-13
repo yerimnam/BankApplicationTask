373ab407957d0341d05bf1a4ca73283c61060d04
yrnam <dpfla725@naver.com>
2023-04-14 08:53:57 +0900

    2023/04/14 -사용자 생성 수정 완료

--- src/main/kotlin/BankApplicationTask/BankApplicationTask/DTO/UsersDTO.kt
+++ src/main/kotlin/BankApplicationTask/BankApplicationTask/DTO/UsersDTO.kt
@@ -5,8 +5,8 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 data class UsersDTO(
     @JsonProperty("user_id")
-    var userId :String?,
-    var password :String?
+    var userId :String,
+    var password :String
     ){
 
     fun toEntity():Users{ //DTO를 Entity로 변환하가 위한 함수
